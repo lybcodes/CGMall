e995f30a5dd913f8f168d626b3dbb28c0b25f6f9
lyb <1462475028@qq.com>
2020-07-21 21:13:52 +0800

    1、雪花算法生成全局唯一id

--- changgou_service/changgou_service_goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/GoodsApplication.java
@@ -1,15 +1,35 @@
 package com.changgou;
 
+import com.changgou.util.IdWorker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan(basePackages = {"com.changgou.goods.dao"})
 public class GoodsApplication {
+
+    @Value("${workerId}")
+    private long workerId;
+
+    @Value("${datacenterId}")
+    private long datacenterId;
+
+
     public static void main(String[] args) {
         SpringApplication.run( GoodsApplication.class);
+    }
+
+    /**
+     * 初始化分布式ID生成器雪花算法，交给spring管理
+     * @return
+     */
+    @Bean
+    public IdWorker initIdWorker(){
+        return new IdWorker(workerId, datacenterId);
     }
 }
