76a109aa19ac22b79840adc9df3c6a89c32a55dc
lyb <1462475028@qq.com>
2020-08-09 16:54:09 +0800

    1、使用canal进行数据库bin_log日志监听

--- changgou_canal/src/main/java/com/itheima/canal/listener/BusinessListener.java
+++ changgou_canal/src/main/java/com/itheima/canal/listener/BusinessListener.java
@@ -20,13 +20,13 @@ public class BusinessListener {
         System.err.println("广告数据发生变化");
 
         //修改前数据
-        for(CanalEntry.Column column: rowData.getBeforeColumnsList()) {
+/*        for(CanalEntry.Column column: rowData.getBeforeColumnsList()) {
             if(column.getName().equals("position")){
                 System.out.println("发送消息到mq  ad_update_queue:"+column.getValue());
                 rabbitTemplate.convertAndSend("","ad_update_queue",column.getValue());  //发送消息到mq
                 break;
             }
-        }
+        }*/
 
         //修改后数据
         for(CanalEntry.Column column: rowData.getAfterColumnsList()) {
