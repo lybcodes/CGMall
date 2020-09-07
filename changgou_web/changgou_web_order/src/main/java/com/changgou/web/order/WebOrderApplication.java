4e8906084b8597e2745f74fa9172e3e3f49cff9e
lyb <1462475028@qq.com>
2020-09-07 23:47:56 +0800

    购物车页面渲染

---
+++ changgou_web/changgou_web_order/src/main/java/com/changgou/web/order/WebOrderApplication.java
@@ -0,0 +1,15 @@
+package com.changgou.web.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.changgou.order.feign"})
+public class WebOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebOrderApplication.class,args);
+    }
+}
